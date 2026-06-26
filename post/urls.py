1c750c6cbbaccefa5d36cb2b46600add7edc10f1
sourav <sourav.mte@gmail.com>
2026-06-27 01:13:01 +0600

    implemented templating

--- post/urls.py
+++ post/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('home/', views.home,name="Home page"),
-    path('posts/<int:id>/',views.dynamic_view,name="dynamic page"),
+    path('home/', views.home,name="home"),
+    path('post_<int:id>/',views.dynamic_view,name="dynamic_view"),
     
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse ,HttpResponseRedirect
+from django.http import HttpResponse, Http404
 
 posts = [
     {
@@ -30,20 +30,23 @@ posts = [
 ]
 
 def home(request):
-
-    return render(request,'home.html')
+    # Pass the entire list of dictionaries to the template
+    context = {'posts': posts}
+    return render(request, 'post/home.html', context)
 
 
 def dynamic_view(request, id):
+    # 1. Search for the specific post
+    selected_post = None
     for post in posts:
         if post["id"] == id:
-            html = f"""
-            <div>
-                <h1>{post['title']}</h1>
-                <p>{post['content']}</p>
-            </div>
-            """
-            return HttpResponse(html)
-
-    return HttpResponse("<h1>Page not found</h1>", status=404)
-
+            selected_post = post
+            break
+            
+    # 2. If no post is found, show a standard 404 error
+    if selected_post is None:
+        raise Http404("Post not found")
+        
+    # 3. If found, package it up and send it to the template!
+    context = {'post': selected_post}
+    return render(request, 'post/post.html', context)
